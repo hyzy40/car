3ee548036898f7ce066a7fff79461dc52a41d812
lebron james <2287680138@qq.com>
2020-10-16 16:22:28 +0800

    put files in the place

---
+++ test_Car.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from car.Car import Car
+
+
+class TestCar(TestCase):
+    def setUp(self):
+        self.car = Car()
+
+
+class TestBrake(TestCar):
+    def test_brake(self):
+        self.car.brake()
+
+        self.assertEqual(self.car.speed, 0)
+
+    def test_accelerate_brake(self):
+        self.car.accelerate()
+
+        self.assertEqual(self.car.speed, 5)
+
+        self.assertEqual(self.car.speed, 5)
+
